3aa301508ae4d17317f959ce64b148950798f988
HamidReza Bashiri <HamidRezaBashiri@protonmail.com>
2022-06-14 10:07:24 +0430

    feature/dependency injection

---
+++ app/src/main/java/com/hamidrezabashiri/ezcard/di/AppModule.kt
@@ -0,0 +1,10 @@
+package com.hamidrezabashiri.ezcard.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.internal.managers.ApplicationComponentManager
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+}
